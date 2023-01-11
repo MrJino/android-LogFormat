902622f5c3539967d6f5f44751fffe3036724c5a
Jinil.Noh <jinil.noh@smartscore.kr>
2023-01-11 10:08:32 +0900

    maven repository 정보변경 & java 소스에서 direct 접근가능하도록 변경

--- utils/src/main/java/noh/jinil/android/utils/log/LogFormat.kt
+++ utils/src/main/java/noh/jinil/android/utils/log/LogFormat.kt
@@ -13,10 +13,12 @@ object LogFormat {
 
     private var enable: Boolean = true
 
+    @JvmStatic
     fun initialize(debuggable: Boolean) {
         enable = debuggable
     }
 
+    @JvmStatic
     fun w(tag: String?, log: String?) {
         if (!enable || log == null) {
             return
@@ -24,6 +26,7 @@ object LogFormat {
         Log.w(MAIN_TAG, "[$tag] $log")
     }
 
+    @JvmStatic
     fun i(tag: String?, log: String?) {
         if (!enable || log == null) {
             return
@@ -31,6 +34,7 @@ object LogFormat {
         Log.i(MAIN_TAG, "[$tag] $log")
     }
 
+    @JvmStatic
     fun d(tag: String?, log: String?) {
         if (!enable || log == null) {
             return
@@ -38,6 +42,7 @@ object LogFormat {
         Log.d(MAIN_TAG, "[$tag] $log")
     }
 
+    @JvmStatic
     fun v(log: String?) {
         if (!enable || log == null) {
             return
@@ -45,6 +50,7 @@ object LogFormat {
         Log.v(MAIN_TAG, "  -> $log")
     }
 
+    @JvmStatic
     fun l(tag: String?, log: String?) {
         if (!enable || log == null) {
             return
@@ -52,6 +58,7 @@ object LogFormat {
         Log.v(MAIN_TAG, " => $log in [$tag]")
     }
 
+    @JvmStatic
     fun e(log: String?) {
         if (!enable || log == null) {
             return
@@ -59,14 +66,20 @@ object LogFormat {
         Log.e(MAIN_TAG, log)
     }
 
+    @JvmStatic
     fun send(data: String) {
-        json("Send", data)
+        printJson("Send", data)
     }
 
+    /**
+     * Json 데이터 파싱
+     */
+    @JvmStatic
     fun receive(data: String) {
-        json("Receive", data)
+        printJson("Receive", data)
     }
 
+    @JvmStatic
     fun httpResponse(message: String) {
         try {
             message.toByteArray(Charsets.UTF_8)
@@ -76,7 +89,7 @@ object LogFormat {
                     Log.d(BODY_TAG, message)
                 }
                 message.startsWith("{") || message.startsWith("[") -> {
-                    json("HTTP", message)
+                    printJson("HTTP", message)
                 }
                 else ->
                     Log.d(HEAD_TAG, message)
@@ -86,7 +99,8 @@ object LogFormat {
         }
     }
 
-    private fun json(title: String, data: String) {
+    private fun printJson(title: String, data: String) {
+        val builder = StringBuilder()
         if (!enable) {
             return
         }
@@ -100,56 +114,94 @@ object LogFormat {
         val delimiter1 = " -------| $title |-----------------------------------------------------------"
         val delimiter2 = delimiter1.replace("[^-]".toRegex(), "-")
 
-        Log.v(BODY_TAG, delimiter1)
+        builder.appendLine()
+        builder.appendLine(delimiter1)
         when (json) {
             is JSONObject -> handleObject(json)
             is JSONArray -> handleArray(json)
+            else -> null
+        }?.let {
+            builder.append(it)
         }
-        Log.v(BODY_TAG, delimiter2)
+        builder.appendLine(delimiter2)
+        Log.v(BODY_TAG, builder.toString())
     }
 
-    private fun handleObject(obj: JSONObject?, blank: String = "") {
-        obj ?: return
+    private fun handleObject(obj: JSONObject?, blank: String = ""): String? {
+        obj ?: return null
+        val builder = StringBuilder()
 
         obj.keys().forEach { key ->
             obj[key].let { value->
                 when (value) {
                     is JSONObject -> {
-                        Log.v(BODY_TAG, "|$blank $key: {")
-                        handleObject(value, "$blank  ")
-                        Log.v(BODY_TAG, "|$blank }")
+                        builder.appendLine("|$blank $key: {")
+                        builder.append(handleObject(value, "$blank  "))
+                        builder.appendLine("|$blank }")
                     }
                     is JSONArray -> {
                         if (value.length() == 0) {
-                            Log.v(BODY_TAG, "|$blank \"$key\": []")
+                            builder.appendLine("|$blank \"$key\": []")
                         } else {
-                            Log.v(BODY_TAG, "|$blank \"$key\":")
-                            handleArray(value, blank)
+                            builder.appendLine("|$blank \"$key\":")
+                            builder.append(handleArray(value, blank))
                         }
                     }
                     else -> {
-                        Log.v(BODY_TAG, "|$blank \"$key\": \"$value\"")
+                        builder.appendLine("|$blank \"$key\": \"$value\"")
                     }
                 }
             }
         }
-
+        return builder.toString()
     }
 
-    private fun handleArray(array: JSONArray, blank: String = "") {
-        Log.v(BODY_TAG, "| $blank[")
-        repeat(array.length()) { i ->
-            when (val item = array.get(i)) {
-                is JSONObject -> {
-                    Log.v(BODY_TAG, "|  $blank{")
-                    handleObject(item, "$blank   ")
-                    Log.v(BODY_TAG, "|  $blank}")
-                }
-                else -> {
-                    Log.v(BODY_TAG, "|  $blank \"$item\"")
+    private fun handleArray(array: JSONArray, blank: String = ""): String {
+        val builder = StringBuilder()
+
+        fun handleOneLine() {
+            builder.append("|$blank - [")
+            repeat(array.length()) { i ->
+                builder.append("\"${array.get(i)}\"")
+                if (i != array.length() - 1) {
+                    builder.append(", ")
                 }
             }
+            builder.appendLine("]")
         }
-        Log.v(BODY_TAG, "| $blank]")
+
+        fun handleSeparate() {
+            builder.appendLine("|  $blank[")
+            repeat(array.length()) { i ->
+                when (val item = array.get(i)) {
+                    is JSONObject -> {
+                        builder.appendLine("|  $blank{")
+                        builder.append(handleObject(item, "$blank   "))
+                        builder.appendLine("|  $blank}")
+                    }
+                    else -> {
+                        builder.appendLine("|  $blank \"$item\"")
+                    }
+                }
+            }
+            builder.appendLine("|  $blank]")
+        }
+
+        when (val sample = array.get(0)) {
+            is Int, is Float -> {
+                handleOneLine()
+            }
+            is String -> {
+                if (sample.length < 20) {
+                    handleOneLine()
+                } else {
+                    handleSeparate()
+                }
+            }
+            else -> {
+                handleSeparate()
+            }
+        }
+        return builder.toString()
     }
 }
